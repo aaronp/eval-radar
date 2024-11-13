13d85a9e20d90c74babdb1f8fed97598777f02b2
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-11-13 12:38:40 +0000

    initial radar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+    import Radar from "$lib/Radar.svelte"
+</script>
+
 <h1>Welcome to eval-radar</h1>
 
 <ol>
     <li><a href="/foo">Foo</a></li>
     <li><a href="/bar">Bar</a></li>
 </ol>
+
+<Radar />
